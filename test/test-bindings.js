f6543dd1ac7f360a4350ba9d0706c7bcbba59575
Patrik Åkerfeldt <patrik.akerfeldt@gmail.com>
2015-07-15 21:56:37 +0200

    Add configx component for configurations
    configx is responsible for reading configuration files.

--- test/test-bindings.js
+++ test/test-bindings.js
@@ -3,11 +3,3 @@ var App = require('../lib/Lovi.js');
 var fs = require('fs');
 
 var lovi = new App();
-
-console.log('C: ' + C.DIR_BINDINGS_CONFIG);
-
-var files = fs.readdirSync(C.DIR_BINDINGS_CONFIG);
-files.forEach(function(file) {
-  console.log('File: ' + file);
-  console.log(JSON.parse(fs.readFileSync(C.DIR_BINDINGS_CONFIG + file, 'utf8')));
-}.bind(this));
